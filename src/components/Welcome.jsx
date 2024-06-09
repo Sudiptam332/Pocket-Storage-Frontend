9c85eb77c0c0e21b30183b2da8a9ff5794253773
Sudipta Mondal <sudiptam332@gmail.com>
2024-06-09 19:41:34 +0530

    modified:   src/components/About.jsx 	modified:   src/components/Welcome.jsx

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -17,8 +17,7 @@ const Welcome = () => {
                 </div>
                 <div className="welcomeRight">
                     <h2>Pocket Storage</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, nisi beatae? Mollitia odit sunt in voluptatem quaerat ullam nisi debitis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, nisi beatae? Mollitia odit sunt in voluptatem quaerat ullam nisi debitis.
-                    </p>
+                    <p>Welcome to Pocket Storage, your go-to digital organizer. Seamlessly save notes, store images, and manage documents all in one secure and convenient platform. Whether you're at home or on the go, Pocket Storage keeps your digital life streamlined and accessible. Experience the ease and security of Pocket Storage.</p>
                     <Link to="/signup" class="btn btn-light">Create Your Account</Link>
                 </div>
             </div>
@@ -29,7 +28,7 @@ const Welcome = () => {
                 </div>
                 <div className="welNoteRight">
                     <h2>Notebook</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, nisi beatae? Mollitia odit sunt in voluptatem quaerat ullam nisi debitis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, nisi beatae? Mollitia odit sunt in voluptatem quaerat ullam nisi debitis.
+                    <p>With Pocket Storage, capturing your thoughts and ideas has never been easier. Our note-saving feature ensures your important notes are always accessible and organized. Jot down reminders, brainstorm new ideas, or keep a daily journal. Whatever you need to remember, Pocket Storage has got you covered, keeping your notes safe and at your fingertips.
                     </p>
                 </div>
             </div>
@@ -40,7 +39,7 @@ const Welcome = () => {
                 </div>
                 <div className="welNoteRight">
                     <h2>Galary</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, nisi beatae? Mollitia odit sunt in voluptatem quaerat ullam nisi debitis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, nisi beatae? Mollitia odit sunt in voluptatem quaerat ullam nisi debitis.
+                    <p>Store and manage your images effortlessly with Pocket Storage. Upload your favorite photos, organize them into albums, and access them anytime, anywhere. Preserve your memories with high-quality image storage, and share your favorite moments with friends and family. With Pocket Storage, your photos are always just a click away.
                     </p>
                 </div>
             </div>
@@ -51,7 +50,7 @@ const Welcome = () => {
                 </div>
                 <div className="welNoteRight">
                     <h2>Document</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, nisi beatae? Mollitia odit sunt in voluptatem quaerat ullam nisi debitis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, nisi beatae? Mollitia odit sunt in voluptatem quaerat ullam nisi debitis.
+                    <p>Keep your important documents safe and well-organized with Pocket Storage. Our platform provides secure storage and easy access to all your files whenever you need them. From work documents to personal files, manage everything in one place with confidence. Enjoy peace of mind knowing your documents are securely stored and readily available when you need them.
                     </p>
                 </div>
             </div>
